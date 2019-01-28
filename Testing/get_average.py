536ffd8a302449ddd39f692fa9538888c69f1026
dyllew3 <digitald83@gmail.com>
2019-01-28 13:52:23 +0000

    Beginnings of averaging

--- Testing/get_average.py
+++ Testing/get_average.py
@@ -1,1 +1,42 @@
 import numpy as np
+from numpy import genfromtxt
+import csv
+
+def time_diff_update(array):
+    result = []
+    i = 1
+    arr_len = len(array)
+    while i + 2 < arr_len:
+        if array[i][6] == "Application Data":
+            sent_diff = float(array[i + 1][1]) - float(array[i][1])
+            second_diff = float(array[i + 2][1]) - float(array[i][1])
+            third_diff = float(array[i + 2][1]) - float(array[i + 1][1])
+            result.append([sent_diff, second_diff, third_diff])
+            i += 3
+        else:
+            i += 1
+    return result
+
+
+
+def get_time_diff(array):
+    result = []
+    i = 1
+    arr_len = len(array)
+    while i + 1 < arr_len:
+        if array[i][6] == "Application Data":
+            result.append((float(array[i + 1][1]) - float(array[i][1])))
+            i += 2
+        else:
+            i += 1
+    return result
+
+with open('Timing-Operations-Results/12-Rules-Issued-Domain.csv') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',')
+    rows = []
+    for row in spamreader:
+        rows.append(row)
+    al = np.array(time_diff_update(rows))
+    print('\n'.join([str(x) for x in al]))
+    print("Average is {}".format(np.mean(al, axis=0)))
+    print("Median is {}".format(np.median(al, axis=0)))
--- Testing/time_operations.py
+++ Testing/time_operations.py
@@ -1,6 +1,9 @@
 NUM_TESTS = 1000
 import json
 import requests
+import time
+URL = "https://34.242.175.35"
+
 
 rating_update = {
         "nymRating" : {
@@ -22,42 +25,43 @@ def make_requests(req, put_req=False, dat=None):
                 s.put(req, data=json.dumps({'rating' : rating_update}), headers=headers, verify=False)
                 rating_update["nymRating"]["numVotes"] += 1
             print(i)
+            time.sleep(0.2)
     return 0
 
 def main():
     # Nyms
-    #make_requests('https://localhost:4400/nym')
-    #make_requests('https://localhost:4400/nym/0')
+    #make_requests(URL + ':4400/nym')
+    #make_requests(URL + ':4400/nym/0')
 
     # Ratings
-    make_requests('https://localhost:4400/ratings/3/spotify.com')
-    # make_requests('https://localhost:4400/ratings/update', True, rating_update)
+    #make_requests(URL + ':4400/ratings/3/spotify.com')
+    #make_requests(URL + ':4400/ratings/update', True, rating_update)
 
     # Cookies
-    # make_requests('https://localhost:4400/cookies/1')
-    # make_requests('https://localhost:4400/cookies/1/spotify.com')
-    # make_requests('https://localhost:4400/cookies/issued/1/spotify.com')
+    #make_requests(URL + ':4400/cookies/1')
+    #make_requests(URL + ':4400/cookies/1/spotify.com')
+    #make_requests(URL + ':4400/cookies/issued/1/spotify.com')
 
     # Rules
-    # make_requests('https://localhost:4400/rules/supported')
-    # make_requests('https://localhost:4400/rules/supported/version')
-    # make_requests('https://localhost:4400/rules/top/3')
-    # make_requests('https://localhost:4400/rules/spotify.com')
-    # make_requests('https://localhost:4400/rules/issued/spotify.com')
+    #make_requests(URL + ':4400/rules/supported')
+    #make_requests(URL + ':4400/rules/supported/version')
+    #make_requests(URL + ':4400/rules/top/3')
+    #make_requests(URL + ':4400/rules/spotify.com')
+    #make_requests(URL + ':4400/rules/issued/spotify.com')
 
     # Identity
-    # make_requests('https://localhost:4400/identity/spotify.com')
+    #make_requests(URL + ':4400/identity/spotify.com')
 
     # Not working
-    # make_requests('https://localhost:4400/identity/spotify.com/timestamp')
+    # make_requests(URL + ':4400/identity/spotify.com/timestamp')
     
-    # make_requests('https://localhost:4400/identity/nym/spotify.com/3')
-    
-    # Not working
-    # make_requests('https://localhost:4400/identity/nym/spotify.com/3/timestamp')
+    make_requests(URL + ':4400/identity/nym/spotify.com/3')
     
     # Not working
-    # make_requests('https://localhost:4400/identity/map/:domain/:username')
+    # make_requests(URL + ':4400/identity/nym/spotify.com/3/timestamp')
+    
+    # Not working
+    # make_requests(URL + ':4400/identity/map/:domain/:username')
 
     return 0
 
