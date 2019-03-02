e1bafe927c60173e9398eb57027bd4cdb85fcb1a
dyllew3 <digitald83@gmail.com>
2019-03-02 19:25:11 +0000

    more logging

--- nym-server-master/nym_server/mix.exs
+++ nym-server-master/nym_server/mix.exs
@@ -37,6 +37,7 @@ defmodule NymServer.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
+     {:logger_file_backend, "~> 0.0.10"},
      {:cowboy, "~> 1.0"}]
   end
 
